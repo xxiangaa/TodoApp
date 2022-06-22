5ff1dba35710190e371f3c025c63db8fe7f35e2c
Maxine <43437731+xxiangaa@users.noreply.github.com>
2022-06-22 16:19:49 +0800

    add file
    add file

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -7,9 +7,8 @@ final class AppTests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        try app.test(.GET, "hello", afterResponse: { res in
+        try app.test(.GET, "health", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
 }
